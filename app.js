4d54eb6018e556abe876f1d2b2f92d0372bb1cfa
Shayekhzaman <ashikzaman149@gmail.com>
2020-08-17 18:17:38 +0600

    now lyrics is available

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let arrayArtist=[];
 let arrayTitle=[];
-
-document.getElementById("searchButton").addEventListener("click",function(){
+// search Button
+document.getElementById("searchButton").addEventListener("click",function (){
+    
 
     const songList=document.getElementById("songList");
     songList.style.display="block";
 
    const songName= document.getElementById("textBox").value;
 
+//    song List From api
    fetch(`https://api.lyrics.ovh/suggest/${songName}`)
    .then(res => res.json())
    .then(data => {
@@ -22,44 +24,53 @@ document.getElementById("searchButton").addEventListener("click",function(){
         arrayArtist.push(artist);
         arrayTitle.push(title);
 
-        const imageLink=element.album.cover;
+    
         
+        // song title and album by
         document.getElementsByTagName("h3")[i].innerHTML=title;
         document.getElementsByClassName("singerName")[i].innerHTML=artist;
 
-
+        // image Link
+        const imageLink=element.album.cover;
         document
             .getElementsByClassName("image")[i]
             .setAttribute('src', `${imageLink}`);
+
+            // from getLyrics Button
+            getLyricsBtn= function(n){
+                const songList=document.getElementById("songList");
+                songList.style.display="none";
+
+                const songLyrics=document.getElementById("songLyrics");
+                songLyrics.style.display="block";
+
+                const artist=arrayArtist[n];
+                const title=arrayTitle[n];
+
+                // song Lyrics
+                fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+                .then(res => res.json())
+                .then(data => {
+                    document.getElementById("singleLyricsTitle").innerText=`${title} ${"--Song by:"} ${artist}`;
+                    if(data.error){
+                        alert("Sorry,Here is no Lyrics! Try another one")
+                    }
+                    else{
+                        document.getElementById("lyrics").innerText=data.lyrics;
+                        console.log(data);
+                    }
+                    
+                    
+                })
+                
+              
+            }
        
     }
+   
 
-    // lyrics display
-
-    document.getElementById("one").addEventListener("click",function(){
-        // console.log(value);
-    //     console.log(arrayTitle[1]);
-    // console.log(arrayArtist[1]);
-    // console.log(arrayArtist[2]);
-    // console.log(arrayArtist[3]);
-    const artist=arrayArtist[0];
-    const title=arrayTitle[0];
-    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById("lyrics").innerText=data;
-        const songLyrics=document.getElementById("songLyrics");
-        songList.style.display="block";
-        console.log(data);
-    })
-
-    })
-    
-
-    
    })
-
- 
-   console.log(songName);
   
 })
+
+
